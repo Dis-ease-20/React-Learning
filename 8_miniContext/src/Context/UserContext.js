44dfa21e607d77124057d214742191a0ff611e90
Vinita Pal <vinita.pal1@OneBanc.ai>
2024-09-13 07:46:11 +0530

    Added themeSwithcer

---
+++ 8_miniContext/src/Context/UserContext.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+//Here userContxt becoame provider which provide the value globaly
+{/* <UserContext>
+    <login />
+    <Card>
+        <Data />
+    </Card>
+    
+</UserContext> */}
